53d56558df903e5cf7ea0314e110ceff7bd8093d
Akhil_Persistent <100367429+akhilpe@users.noreply.github.com>
2022-03-07 12:53:06 +0530

    Random password generation

--- Assignmentday2.tf
+++ Assignmentday2.tf
@@ -4,7 +4,7 @@
 resource "random_password" "password" {
   length           = 8
   special          = true
-  override_special = "!#$%&*()"
+  override_special = "@"
   min_lower        = 1
   min_upper        = 1
   min_special      = 1
